854cadbb535c5dc740c2edf2e7181114215e1f4d
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-03-28 20:22:24 +0530

    refactor: moved project to apps/website

--- apps/website/src/app/events/components/event-image-carousel.tsx
+++ apps/website/src/app/events/components/event-image-carousel.tsx
@@ -109,7 +109,7 @@ const EventsImageCarousel = () => {
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
-              stroke-width="2"
+              strokeWidth="2"
               d="M5 1 1 5l4 4"
             />
           </svg>
@@ -133,7 +133,7 @@ const EventsImageCarousel = () => {
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
-              stroke-width="2"
+              strokeWidth="2"
               d="m1 9 4-4-4-4"
             />
           </svg>
