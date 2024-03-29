1f34d53bd053874489f8c8041fb66b046e59da9f
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-03-29 12:03:39 +0530

    add events and admin code

---
+++ packages/types/index.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+export const eventFormSchema = z.object({
+  eventName: z
+    .string()
+    .min(2, {
+      message: "Event name must be at least 2 characters.",
+    })
+    .max(50, {
+      message: "Event name must not be longer than 50 characters.",
+    }),
+  eventDescription: z.string().max(200, {
+    message: "Event description must not be longer than 200 characters.",
+  }),
+  eventStartDateTime: z.string({
+    required_error: "Please select a start date and time for the event.",
+  }),
+  eventFinishDateTime: z.string({
+    required_error: "Please select a finish date and time for the event.",
+  }),
+  eventVenue: z.string().max(100, {
+    message: "Event venue must not be longer than 100 characters.",
+  }),
+  organizedClub: z.string().max(50, {
+    message: "Please select the club organizing the event.",
+  }),
+  eventImage: z.string().url({
+    message: "Event image must be a valid URL.",
+  }),
+  contactMembersDetails: z.string().max(200, {
+    message: "Contact members must be selected.",
+  }),
+  isTeamEvent: z.boolean(),
+  maxTeamMembers: z.number().min(1, {
+    message: "Maximum team members must be at least 1.",
+  }),
+});
+
+export type EventFormType = z.infer<typeof eventFormSchema>;
