d4d29dcc27bd4db69f72c5c20c5a5a3eb70d6fb2
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-03-30 21:23:16 +0530

    initialised firebase using init command and emulators

--- apps/functions/.eslintrc.js
+++ apps/functions/.eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
-  extends: ["@repo/eslint-config/next.js"],
-  project: path.join(__dirname, "tsconfig.json"),
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+  },
 };
