25dab003254e8981986a9f1bbaca24dad1c919e6
Tushar Agrawal <tushar.code.inbox@gmail.com>
2024-04-06 13:14:32 +0530

    declare winner ui

--- apps/web/app/admin/events/winner/page.tsx
+++ apps/web/app/admin/events/winner/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { Button } from "@repo/ui/components/ui/button";
+import Link from "next/link";
 
-const Winner = () => {
-  return <div>Winner</div>;
-};
-
-export default Winner;
+export default function WinnersPage() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 px-4 lg:gap-6 lg:px-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-lg font-semibold md:text-2xl">Declare Winner</h1>
+        <Button asChild variant={"outline"}>
+          <Link
+            href="/admin/events/winner/previous"
+            className="hidden min-[475px]:block"
+          >
+            Previous Winners
+          </Link>
+        </Button>
+      </div>
+      <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm px-4 sm:px-0">
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h3 className="text-2xl font-bold tracking-tight">
+            Want to declare a winner?
+          </h3>
+          <p className="text-sm text-muted-foreground">
+            Click on the button below to declare the winner of the event.
+          </p>
+          <div className="flex gap-4 items-center justify-center mt-4 flex-col min-[475px]:flex-row">
+            <Button>Grand Prize</Button>
+            <Button variant={"outline"}>Honorable Mentions</Button>
+            <Button
+              asChild
+              variant={"outline"}
+              className="block min-[475px]:hidden"
+            >
+              <Link href="/admin/events/winner/previous">Previous Winners</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/web/app/admin/events/winner/previous/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import RecentWinner from "../../components/recent-winner";
+import { Search } from "lucide-react";
+import { Input } from "@repo/ui/components/ui/input";
+
+const PreviousWinnersPage = () => {
+  return (
+    <div className="container">
+      <h1 className="text-lg font-semibold md:text-2xl">
+        Recent Event Winners
+      </h1>
+      <RecentWinner />
+      <div className="mt-8">
+        <h1 className="text-lg font-semibold md:text-2xl">
+          Previous Event Winners
+        </h1>
+        <div className="relative mt-2">
+          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            placeholder="Search events..."
+            className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreviousWinnersPage;
